c297c76e3c3780f8c1c69400bbf250d3fa3f345c
Sanam MAJITHIA <sanam.majithia@amadeus.com>
2020-08-07 04:35:53 +0530

    Updates

---
+++ precache-manifest.85939fe904e22c0fe63474908ec91c8b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e05e3f349eb1d397c4576631baa24e",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "a8b61a0917089a6548b0",
+    "url": "/keeper-app/static/js/2.1078577d.chunk.js"
+  },
+  {
+    "revision": "1f7e5eef868747c9a92e",
+    "url": "/keeper-app/static/js/main.01beaa9b.chunk.js"
+  },
+  {
+    "revision": "50dc6f055f932acedbc2",
+    "url": "/keeper-app/static/js/runtime-main.54e84bc1.js"
+  }
+]);
