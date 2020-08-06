4318092064ba78cd3ca3e24102220311133f9fcb
Sanam MAJITHIA <sanam.majithia@amadeus.com>
2020-08-07 04:53:26 +0530

    Handled Header image display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import KeeperLogo from '../res/keep.png';
 
 const heading = "Keeper";
 
 function Header() {
   return (
     <header>
-      <img src="/keep.png" alt="Keeper Icon" />
+      <img src={KeeperLogo} alt="Keeper Icon" />
       <h1>{heading}</h1>
     </header>
   );
